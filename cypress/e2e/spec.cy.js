ce7664689cdaeccf8c72f786c568ee729cb8e4a9
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-09 14:33:02 -0400

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+context("Home Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("Should find our welcome page and message", () => {
+    cy.get("h1").contains("Welcome");
+  });
+});
